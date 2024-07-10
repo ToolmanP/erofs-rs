3529372a6d8a4d6654a041cdc69015d3ea92663e
Yiyang Wu <toolmanp@tlmp.cc>
2024-07-11 00:27:29 +0800

    feat(lib): some type infos
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- erofs-sys/src/lib.rs
+++ erofs-sys/src/lib.rs
@@ -3,11 +3,17 @@
 #[cfg(feature = "alloc")]
 extern crate alloc;
 
+pub(crate) const EROFS_BLOCK_SZ: usize = 4096;
+pub(crate) const EROFS_EMPTY_BLOCK: Block = [0; EROFS_BLOCK_SZ];
+
+pub type Block = [u8; EROFS_BLOCK_SZ];
 pub type Blk = u32;
-pub type Offset = u64;
+pub type Off = u64;
 pub type Nid = u64;
 
-mod superblock;
 mod compression;
-mod inode;
+mod data;
 mod dir;
+mod map;
+mod inode;
+mod superblock;
