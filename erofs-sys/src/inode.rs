5f74263599181d260a75198ee24b0e9bd9259daa
Yiyang Wu <toolmanp@tlmp.cc>
2024-07-11 00:27:58 +0800

    feat(inode): impl layout/format
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- erofs-sys/src/inode.rs
+++ erofs-sys/src/inode.rs
@@ -1,41 +1,121 @@
-use crate::Nid;
-#[repr(C)]
-pub struct CompactInode {
-    pub i_format: u16,
-    pub i_xattr_icount: u16,
-    pub i_mode: u16,
-    pub i_nlink: u16,
-    pub i_size: u32,
-    pub i_reserved: [u8; 4],
-    pub i_u: [u8; 4],
-    pub i_ino: u32,
-    pub i_uid: u16,
-    pub i_gid: u16,
-    pub i_reserved2: [u8; 4],
+use crate::data::Backend;
+use crate::superblock::SuperBlockInfo;
+use crate::*;
+
+use core::mem::size_of;
+
+pub(crate) struct NameiContext {
+    pub(crate) nid: Nid,
+    pub(crate) ftype: u32,
 }
 
 #[repr(C)]
-pub struct ExtendedInode {
-    pub i_format: u16,
-    pub i_xattr_icount: u16,
-    pub i_mode: u16,
-    pub i_reserved: [u8; 2],
-    pub i_size: u64,
-    pub i_u: [u8; 4],
-    pub i_ino: u32,
-    pub i_uid: u32,
-    pub i_gid: u32,
-    pub i_mtime: u64,
-    pub i_mtime_nsec: u32,
-    pub i_nlink: u32,
-    pub i_reserved2: [u8; 16],
+#[derive(Clone, Copy)]
+pub(crate) struct Format(u16);
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub(crate) enum Version {
+    Compat,
+    Extended,
+    Unknown,
 }
 
-pub enum GenericInode {
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub(crate) enum Layout {
+    FlatPlain,
+    CompressedFull,
+    FlatInline,
+    CompressedCompact,
+    Chunk,
+    Unknown,
+}
+
+pub(crate) enum Type {
+    Regular,
+    Directory,
+    Link,
+    Character,
+    Block,
+    FIFO,
+    Socket,
+}
+
+impl Format {
+    pub(crate) fn version(&self) -> Version {
+        match (self.0 >> 0) & ((1 << 1) - 1) {
+            0 => Version::Compat,
+            1 => Version::Extended,
+            _ => Version::Unknown,
+        }
+    }
+
+    pub(crate) fn layout(&self) -> Layout {
+        match (self.0 >> 1) & ((1 << 3) - 1) {
+            0 => Layout::FlatPlain,
+            1 => Layout::CompressedFull,
+            2 => Layout::FlatInline,
+            3 => Layout::CompressedCompact,
+            4 => Layout::Chunk,
+            _ => Layout::Unknown,
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub(crate) struct CompactInode {
+    pub(crate) i_format: Format,
+    pub(crate) i_xattr_icount: u16,
+    pub(crate) i_mode: u16,
+    pub(crate) i_nlink: u16,
+    pub(crate) i_size: u32,
+    pub(crate) i_reserved: [u8; 4],
+    pub(crate) i_u: [u8; 4],
+    pub(crate) i_ino: u32,
+    pub(crate) i_uid: u16,
+    pub(crate) i_gid: u16,
+    pub(crate) i_reserved2: [u8; 4],
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub(crate) struct ExtendedInode {
+    pub(crate) i_format: Format,
+    pub(crate) i_xattr_icount: u16,
+    pub(crate) i_mode: u16,
+    pub(crate) i_reserved: [u8; 2],
+    pub(crate) i_size: u64,
+    pub(crate) i_u: [u8; 4],
+    pub(crate) i_ino: u32,
+    pub(crate) i_uid: u32,
+    pub(crate) i_gid: u32,
+    pub(crate) i_mtime: u64,
+    pub(crate) i_mtime_nsec: u32,
+    pub(crate) i_nlink: u32,
+    pub(crate) i_reserved2: [u8; 16],
+}
+
+#[derive(Clone, Copy)]
+pub(crate) enum GenericInode {
     Extended(ExtendedInode),
     Compact(CompactInode),
 }
 
+#[derive(Clone, Copy)]
+pub(crate) struct ChunkSpec {
+    chunkformat: u16,
+    chunkbits: u8,
+}
+
+#[derive(Clone, Copy)]
+pub(crate) enum Spec {
+    Chunk(ChunkSpec),
+    Data(u32),
+    Device(u32),
+    Unknown,
+}
 
 impl GenericInode {
     pub fn ino(&self) -> u32 {
@@ -44,11 +124,134 @@ impl GenericInode {
             Self::Compact(compact) => compact.i_ino,
         }
     }
+
+    pub fn format(&self) -> Format {
+        match self {
+            Self::Extended(extended) => extended.i_format,
+            Self::Compact(compact) => compact.i_format,
+        }
+    }
+
+    pub fn size(&self) -> Off {
+        match self {
+            Self::Extended(extended) => extended.i_size,
+            Self::Compact(compact) => compact.i_size as u64,
+        }
+    }
+
+    pub fn inode_size(&self) -> Off {
+        match self {
+            Self::Extended(_) => 64,
+            Self::Compact(_) => 32,
+        }
+    }
+
+    pub fn xattr_size(&self) -> Off {
+        match self {
+            Self::Extended(extended) => 5 + 4 * (extended.i_xattr_icount as u64 - 1),
+            Self::Compact(_) => 0,
+        }
+    }
+
+    pub fn spec(&self) -> Spec {
+        let mode = match self {
+            Self::Extended(extended) => extended.i_mode,
+            Self::Compact(compact) => compact.i_mode,
+        };
+
+        let u = match self {
+            Self::Extended(extended) => &extended.i_u,
+            Self::Compact(compact) => &compact.i_u,
+        };
+
+        match mode & 0o170000 {
+            0o40000 => Spec::Data(u32::from_le_bytes(*u)), // Directory
+            0o100000 => Spec::Data(u32::from_le_bytes(*u)), // Regular File
+            0o120000 => Spec::Data(u32::from_le_bytes(*u)), // Symbolic Link
+            0o10000 => Spec::Device(0),                    // FIFO
+            0o140000 => Spec::Device(0),                   // Socket
+            0o60000 => unimplemented!(),                   // Block
+            0o20000 => unimplemented!(),                   // Character
+            _ => Spec::Unknown,
+        }
+    }
 }
 
+pub(crate) type InodeBuf = [u8; size_of::<ExtendedInode>()];
+pub(crate) const DEFAULT_INODE_BUF: InodeBuf = [0; size_of::<ExtendedInode>()];
+
+#[derive(Clone, Copy)]
 pub struct Inode {
-    pub g: GenericInode,
+    pub inner: GenericInode,
     pub nid: Nid,
+}
+
+#[derive(Debug)]
+pub enum InodeError {
+    VersionError,
+    UnknownError,
+}
+
+type InodeResult<T> = Result<T, InodeError>;
+
+impl<'a> TryFrom<&'a [u8]> for &'a CompactInode {
+    type Error = InodeError;
+    fn try_from(value: &'a [u8]) -> Result<Self, Self::Error> {
+        //SAFETY: all the types present are ffi-safe. safe to cast here since only [u8;64] could be
+        //passed into this function and it's definitely safe.
+        let inode: &'a CompactInode = unsafe { &*(value.as_ptr() as *const CompactInode) };
+        let ifmt = &inode.i_format;
+        match ifmt.version() {
+            Version::Compat => Ok(inode),
+            Version::Extended => Err(InodeError::VersionError),
+            _ => Err(InodeError::UnknownError),
+        }
+    }
+}
+
+impl TryFrom<InodeBuf> for GenericInode {
+    type Error = InodeError;
+    fn try_from(value: InodeBuf) -> Result<Self, Self::Error> {
+        let r: Result<&CompactInode, Self::Error> = value.as_slice().try_into();
+        match r {
+            Ok(compact) => Ok(GenericInode::Compact(*compact)),
+            Err(e) => match e {
+                //SAFETY: Note that try_into will return VersionError. This suggests that current
+                //buffer contains the extended inode. Since the types used are FFI-safe, it's safe
+                //to transtmute it here.
+                InodeError::VersionError => Ok(GenericInode::Extended(unsafe {
+                    core::mem::transmute(value)
+                })),
+                _ => Err(e),
+            },
+        }
+    }
+}
+
+impl<T> SuperBlockInfo<T>
+where
+    T: Backend,
+{
+    pub(crate) fn iloc(&self, nid: Nid) -> Off {
+        let sb = &self.sb;
+        self.blkpos(sb.meta_blkaddr as u32) + ((nid as Off) << (sb.meta_blkaddr as Off))
+    }
+
+    fn read_inode(&self, nid: Nid) -> Inode {
+        let offset = self.iloc(nid);
+        let mut buf: InodeBuf = DEFAULT_INODE_BUF;
+        self.backend
+            .fill(&mut buf, offset, size_of::<InodeBuf>() as u64)
+            .unwrap();
+        Inode {
+            inner: GenericInode::try_from(buf).unwrap(),
+            nid,
+        }
+    }
+
+    pub fn namei(&self, name: &str, len: usize) -> Option<NameiContext> {
+        None
+    }
 }
 
 #[cfg(test)]
