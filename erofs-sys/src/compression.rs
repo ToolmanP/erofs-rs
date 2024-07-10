fa69371d484cdd1d904c9da6c4500207d507cb41
Yiyang Wu <toolmanp@tlmp.cc>
2024-07-11 00:09:52 +0800

    feat: add default impl for comprInfo
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- erofs-sys/src/compression.rs
+++ erofs-sys/src/compression.rs
@@ -1,11 +1,17 @@
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
-pub enum CompressionInfo {
+pub enum SuperblockCompressionInfo {
     AvailableComprAlgs(u16),
     Lz4MaxDistance(u16),
 }
 
-impl Default for CompressionInfo{
+
+#[allow(dead_code)]
+pub enum InodeCompressionInfo {
+
+}
+
+impl Default for SuperblockCompressionInfo{
     fn default() -> Self {
         Self::AvailableComprAlgs(0)
     }
