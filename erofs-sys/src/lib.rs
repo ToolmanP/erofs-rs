55eb16abb37356c9dc7c1200bc0d45e35667c27b
Yiyang Wu <toolmanp@tlmp.cc>
2024-07-13 00:37:41 +0800

    chore: add round macros
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- erofs-sys/src/lib.rs
+++ erofs-sys/src/lib.rs
@@ -5,6 +5,7 @@ extern crate alloc;
 
 pub(crate) const EROFS_BLOCK_SZ: usize = 4096;
 pub(crate) const EROFS_EMPTY_BLOCK: Block = [0; EROFS_BLOCK_SZ];
+pub(crate) const EROFS_SUPER_OFFSET: Off = 1024;
 
 pub type Block = [u8; EROFS_BLOCK_SZ];
 pub type Blk = u32;
@@ -17,3 +18,13 @@ mod dir;
 mod map;
 mod inode;
 mod superblock;
+
+#[macro_export]
+macro_rules! round {
+    (UP, $x: expr, $y: expr) => {
+        ($x + $y - 1) / $y * $y
+    };
+    (DOWN, $x: expr, $y: expr) => {
+        ($x / $y) * $y
+    };
+}
